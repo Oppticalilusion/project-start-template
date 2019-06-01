7a8ed4d2e8691dc76dfae83df181f0cb7a39da91
Kevan Rayner <Kevanrayner@gmail.com>
2019-06-01 00:19:14 -0600

    Edits to the main gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,3 @@ var gulp = require('gulp');
 gulp.task('default', function a() {
 	console.log("This is just a default, add more to your gulp request");
 });
-
-gulp.task('html', function a() {
-	console.log("Imagine something useful being done to your html here");
-});
-
